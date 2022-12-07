256856def62c09b040e2398ba679cd234e3a475d
Jean Abarquez <jeankhladyn.abarquez.iics@ust.edu.ph>
2022-12-07 22:39:27 +0800

    Add initial navbar

--- app/src/main/java/com/example/thomedss/PatientInfoActivity.java
+++ app/src/main/java/com/example/thomedss/PatientInfoActivity.java
@@ -1,13 +1,20 @@
 package com.example.thomedss;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class PatientInfoActivity extends AppCompatActivity {
 
@@ -16,11 +23,53 @@ public class PatientInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
 
-        DrawerLayout sidebar = findViewById(R.id.sidebar);
+        final DrawerLayout drawerLayout = findViewById(R.id.layout_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView sidebar = findViewById(R.id.sidebar);
+
+
+
+        //TEST
+        Button testbtn = findViewById(R.id.testbtn);
+
+        testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PatientInfoActivity.this, PersonalActivity.class));
+                Toast.makeText(PatientInfoActivity.this, "Personal", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setSupportActionBar(toolbar);
+
         findViewById(R.id.icon_menu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sidebar.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        sidebar.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getTitle().toString())
+                {
+                    case "Personal and Contact Information":
+                    {
+                        startActivity(new Intent(PatientInfoActivity.this, PersonalActivity.class));
+                        Toast.makeText(PatientInfoActivity.this, "Personal", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    case "Health History":
+                    {
+                        startActivity(new Intent(PatientInfoActivity.this, HealthActivity.class));
+                        break;
+                    }
+
+                }
+
+                return true;
             }
         });
 
--- app/src/main/java/com/example/thomedss/PersonalActivity.java
+++ app/src/main/java/com/example/thomedss/PersonalActivity.java
@@ -1,8 +1,13 @@
 package com.example.thomedss;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class PersonalActivity extends AppCompatActivity {
 
@@ -11,4 +16,11 @@ public class PersonalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
     }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+//
+//        return super.onCreateView(parent, name, context, attrs);
+//    }
 }
